8cb591a1c876b39ebab24151c764dd1a23ce11e6
AICloudStrategist Support <support@aicloudstrategist.com>
2026-05-19 14:02:45 +0530

    Bust navbar logo cache

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { basePath } from '@/lib/siteLinks';
 
-const logoSrc = `${basePath}/brand/zephyr-navbar-logo.png`;
+const logoSrc = `${basePath}/brand/zephyr-navbar-logo-b7ed01a.png`;
 
 const navItems = [
   ['WORK', '#portfolio'],
